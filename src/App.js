a57821450b1be79e8f461fbb336bd234ceecaa68
Carolyn Stransky <carolynstransky@gmail.com>
2020-08-19 00:30:38 +0200

    :fishing_pole_and_fish: Refactor to use hooks

--- src/App.js
+++ src/App.js
@@ -1,70 +1,49 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import birthdayMessages from "./data/birthdayMessages";
 import MessageContainer from "./components/MessageContainer";
 import confetti from "canvas-confetti";
-import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      message: "",
-      name: "",
-      email: "",
-      messageCount: []
-    };
-
-    this.getNextMessage = this.getNextMessage.bind(this);
-  }
-
-  componentDidMount() {
-    this.setState({
-      message: birthdayMessages[0].message,
-      name: birthdayMessages[0].name,
-      email: birthdayMessages[0].email,
-      messageCount: 0
-    });
-  }
-
-  getNextMessage = () => {
-    this.showNextMessage();
-    confetti();
+const App = () => {
+  let initialState = {
+    message: birthdayMessages[0].message,
+    name: birthdayMessages[0].name,
+    email: birthdayMessages[0].email,
+    messageCount: 0
   };
 
-  showNextMessage() {
-    const counter = this.state.messageCount + 1;
+  const [state, setState] = useState(initialState);
 
-    this.setState({
+  const showNextMessage = () => {
+    const counter = state.messageCount + 1;
+
+    setState({
       messageCount: counter,
       message: birthdayMessages[counter].message,
       name: birthdayMessages[counter].name,
       email: birthdayMessages[counter].email
     });
-  }
 
-  render() {
-    return (
-      <div className="App">
-        <div as="header">
-          <h1>Happy 90th Birthday!</h1>
-        </div>
-        <MessageContainer
-          messageCount={this.state.messageCount}
-          message={this.state.message}
-          name={this.state.name}
-          email={this.state.email}
-          messageTotal={birthdayMessages.length}
-          lastMessage={
-            this.state.messageCount + 1 === birthdayMessages.length
-              ? true
-              : false
-          }
-          onNextClick={this.getNextMessage}
-        />
+    confetti();
+  };
+
+  return (
+    <div className="App">
+      <div as="header">
+        <h1>Happy 90th Birthday!</h1>
       </div>
-    );
-  }
-}
+      <MessageContainer
+        messageCount={state.messageCount}
+        message={state.message}
+        name={state.name}
+        email={state.email}
+        messageTotal={birthdayMessages.length}
+        lastMessage={
+          state.messageCount + 1 === birthdayMessages.length ? true : false
+        }
+        onNextClick={showNextMessage}
+      />
+    </div>
+  );
+};
 
 export default App;
