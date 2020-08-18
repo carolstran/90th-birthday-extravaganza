2c1c82acdbecb42fb9182cfb7cc81c8cc18a11d9
Carolyn Stransky <carolynstransky@gmail.com>
2020-08-18 23:53:53 +0200

    :lock: Made the repo private so fuck it

---
+++ src/data/birthdayMessages.js
@@ -0,0 +1,43 @@
+const birthdayMessages = [
+  {
+    message: "Happy birthday to the best grandpa a girl could ask for!",
+    name: "Beth",
+    email: "wakandadr@gmail.com"
+  },
+  {
+    message:
+      "HAPPY 90TH BIRTHDAY GRANDPA JOE !! We hope you have a wonderful day filled with presents, family time and lots of gluten free cake. We wish we could see you on your special day but we will be loving you from afar and look forward to FaceTiming you instead !! Love to you always !! From, Jack and Ella xx",
+    name: "Jack and Ella",
+    email: "jackjpleckinger@gmail.com"
+  },
+  {
+    message: "Happy Birthday Joe!",
+    name: "Bob Tidwell",
+    email: "r3075@comcast.net"
+  },
+  {
+    message:
+      "Happy B-day Joe!!!!!!!You don't look a day older than 89 @ 11 months. Have a good day.",
+    name: "Mel",
+    email: "1golfme@gmail.com"
+  },
+  {
+    message: "Hi Joe, We'll toast you this winter in AZ. All the BEST",
+    name: "George Littlejohn & Elaine Bamford",
+    email: "glittlejohn@shaw.ca"
+  },
+  {
+    message:
+      "Happy happy happy birthday! While there are many days and birthdays in August, this one has to be my favorite. Cheers to the man who taught me how to golf and who introduced me to reading real newspapers. I love you so much and hope you have the best day on your 90th ❤️🎂🎈🎊",
+    name: "Carolyn Stransky",
+    email: "carolynstransky@gmail.com"
+  },
+  {
+    message:
+      "Lieber Joe, Herzlichen Glückwunsch zum 90ten Geburtstag! Ich wünsche dir viel Gesundheit, Glück und Freude für die nächsten Jahre. Ich freue mich schon auf unser nächstes Wiedersehen. Viele Grüße 🎉",
+    name: "Florian Blankenburg",
+    email: "florian.blankenburg@web.de"
+  }
+];
+
+export default birthdayMessages;
