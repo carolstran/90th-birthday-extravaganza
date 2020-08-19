c2d238cffee341c42ee65e2698cac14b22a32ab4
Carolyn Stransky <carolynstransky@gmail.com>
2020-08-19 13:21:17 +0200

    💌 Add new messages

--- src/data/birthdayMessages.js
+++ src/data/birthdayMessages.js
@@ -17,9 +17,21 @@ const birthdayMessages = [
   },
   {
     message:
+      "Hey Joe - Who is that devilishly good looking guy in the high school graduation picture?  Someone must have photoshopped that for you! :)  Lots of great memories of Friday night lights with the Owatonna 'Indians', baseball at Dartts Park and Apache camping trailers.  I could always hear Joe cheering above the crowd at Indian football, basketball and baseball games.  Thanks for all the great memories !!!   Hope you have a special  90th  with your family and a lot of Zoom friends.  You still have a graceful golf swing so I know you must be scoring less than 90 on the links. \n\n Happy Birthday, Craig. \n\n P.S.Could you give Bruce a few golf lessons to get him ready for the next Stimson Cup? :)",
+    name: "Craig Kinney",
+    email: "kinneycem@hotmail.com"
+  },
+  {
+    message:
       "Happy B-day Joe!!!!!!! You don't look a day older than 89 @ 11 months. Have a good day.",
     name: "Mel",
     email: "1golfme@gmail.com"
+  },
+  {
+    message:
+      "Dear Joe, \n\n A very happy birthday and congratulations from the heart for you & your people! \n\n Wonderful pictures of a wonderful life & family!! \n\n For me it was such a big pleasure to meet you and Bird's Legs in Hilton Head, and share very very nice days there with you and all the others. \n\n Take good care and enjoy your day!",
+    name: "Juan R. Telleria (Spain)",
+    email: "jrtelleria@servicopter.com"
   },
   {
     message: "Hi Joe, We'll toast you this winter in AZ. All the BEST",
