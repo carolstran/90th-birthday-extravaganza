d7d47214d17eea8a221de92236575d88a7e75cb3
Carolyn Stransky <carolynstransky@gmail.com>
2020-08-19 13:26:48 +0200

    🪓 Add ability to split name for email

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,33 +1,37 @@
 import React from "react";
 import { Heading, Link, Text } from "@chakra-ui/core";
 
-const Message = props => (
-  <>
-    <Heading
-      as="h2"
-      size="lg"
-      fontWeight={500}
-      textAlign="center"
-      mt={2}
-      mb={6}
-    >
-      {props.message}
-    </Heading>
-    <Heading as="h3" size="md" fontWeight={500} textAlign="center" mb={12}>
-      - {props.name}
-    </Heading>
-    <Text textAlign="center" mb={18}>
-      <span role="img" aria-label="">
-        💌
-      </span>{" "}
-      Email {props.name}:{" "}
-      <Link
-        href={`mailto:${props.email}?subject=Thanks for the birthday wishes!`}
+const Message = props => {
+  const splitName = props.name.split(" ");
+
+  return (
+    <>
+      <Heading
+        as="h2"
+        size="lg"
+        fontWeight={500}
+        textAlign="center"
+        mt={2}
+        mb={6}
       >
-        {props.email}
-      </Link>
-    </Text>
-  </>
-);
+        {props.message}
+      </Heading>
+      <Heading as="h3" size="md" fontWeight={500} textAlign="center" mb={12}>
+        - {props.name}
+      </Heading>
+      <Text textAlign="center" mb={18}>
+        <span role="img" aria-label="">
+          💌
+        </span>{" "}
+        Email {splitName[0]}:{" "}
+        <Link
+          href={`mailto:${props.email}?subject=Thanks for the birthday wishes!`}
+        >
+          {props.email}
+        </Link>
+      </Text>
+    </>
+  );
+};
 
 export default Message;
